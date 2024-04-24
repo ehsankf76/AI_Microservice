5da58baf3e0de4b0bfd07126fcbceab479022d84
ehsankf <ehsan.kf76@gmail.com>
2024-04-25 00:09:13 +0330

    Reinstall venv

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Ehsan Karegar Froogh\Desktop\Traffic_Sign_rRecognition\venv
+set VIRTUAL_ENV=C:\Users\Ehsan Karegar Froogh\Desktop\AI_Microservice\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
